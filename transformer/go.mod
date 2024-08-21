97f509119426087908678057850a154e872275ed
niketanand-7 <niketanand7@gmail.com>
2024-08-21 16:24:29 -0400

    Update dependencies and implement batch generation for training

--- transformer/go.mod
+++ transformer/go.mod
@@ -3,6 +3,11 @@ module transformer
 go 1.23.0
 
 require (
+	golang.org/x/exp v0.0.0-20191002040644-a1355ae1e2c3
+	gorgonia.org/tensor v0.9.24
+)
+
+require (
 	github.com/apache/arrow/go/arrow v0.0.0-20211112161151-bc219186db40 // indirect
 	github.com/awalterschulze/gographviz v2.0.3+incompatible // indirect
 	github.com/chewxy/hm v1.0.0 // indirect
@@ -21,7 +26,6 @@ require (
 	gorgonia.org/cu v0.9.4 // indirect
 	gorgonia.org/dawson v1.2.0 // indirect
 	gorgonia.org/gorgonia v0.9.18 // indirect
-	gorgonia.org/tensor v0.9.24 // indirect
 	gorgonia.org/vecf32 v0.9.0 // indirect
 	gorgonia.org/vecf64 v0.9.0 // indirect
 )
--- transformer/train.go
+++ transformer/train.go
@@ -6,16 +6,17 @@ import (
 	"os"
 	"sort"
 	"strings"
+	"time"
 
-	// "gorgonia.org/gorgonia"
+	"golang.org/x/exp/rand"
+	"gorgonia.org/gorgonia"
 	"gorgonia.org/tensor"
 )
 
 func readFile(fileName string) (string, error) {
-	// Read the file
 	content, err := ioutil.ReadFile(fileName)
 	if err != nil {
-		fmt.Println(err)
+		return "", err
 	}
 	return string(content), nil
 }
@@ -44,12 +45,35 @@ func encodeString(text string, charToIdx map[string]int) []int {
 	return encodedText
 }
 
-func decodeString(encodedText []int, idxToChar map[int]string) string {
-	decodedText := ""
-	for _, idx := range encodedText {
-		decodedText += idxToChar[idx]
+func get_batch(split string, train_data, val_data []int, batch_size, block_size int) (*tensor.Dense, *tensor.Dense) {
+	var data []int
+	if split == "train" {
+		data = train_data
+	} else {
+		data = val_data
 	}
-	return decodedText
+
+	rand.Seed(uint64(time.Now().UnixNano()))
+
+	ix := make([]int, batch_size)
+	for i := range ix {
+		ix[i] = rand.Intn(len(data) - block_size)
+	}
+
+	xData := make([]int, batch_size*block_size)
+	yData := make([]int, batch_size*block_size)
+
+	for i := 0; i < batch_size; i++ {
+		for j := 0; j < block_size; j++ {
+			xData[i*block_size+j] = data[ix[i]+j]
+			yData[i*block_size+j] = data[ix[i]+j+1]
+		}
+	}
+
+	x := tensor.New(tensor.WithShape(batch_size, block_size), tensor.Of(tensor.Int), tensor.WithBacking(xData))
+	y := tensor.New(tensor.WithShape(batch_size, block_size), tensor.Of(tensor.Int), tensor.WithBacking(yData))
+
+	return x, y
 }
 
 func main() {
@@ -59,23 +83,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	// print the length of dataset in characters
-	// fmt.Println("Length of text: ", len(text))
-	// substring := text[:1000]
-	// fmt.Println("Substring: ", substring)
-
-	// sets do not exist in golang, so we will use a map for the characters in text and it will behave as a set
 	vocabSet := map[string]struct{}{}
-
-	// iterate over the text and add each character to the set
 	for _, char := range text {
 		vocabSet[string(char)] = struct{}{}
 	}
 
-	// print the number of unique characters in the text
 	fmt.Println("Number of unique characters: ", len(vocabSet))
 
-	// create a list of unique characters
 	var vocabSlice []string
 	for char := range vocabSet {
 		vocabSlice = append(vocabSlice, char)
@@ -86,27 +100,11 @@ func main() {
 	sortedVocabStr := strings.Join(vocabSlice, "")
 	fmt.Println("Sorted Vocab: ", sortedVocabStr)
 
-	// 	TOKENIZATION!!!
-	// currently using character level tokenizatino, will change to use tiktoken or google subword tokenizer
-	// create a mapping from character to index and index to character
 	stoi := mapCharToIdx(sortedVocabStr)
-	// itos := mapIdxToChar(sortedVocabStr)
-
 	encodedText := encodeString(text, stoi)
-	encodedTensor := tensor.New(tensor.WithShape(len(encodedText)), tensor.Of(tensor.Int), tensor.WithBacking(encodedText))
 
-	fmt.Println("Tensor shape: ", encodedTensor.Shape())
-	fmt.Println("Tensor data type: ", encodedTensor.Dtype())
+	fmt.Println("Tensor shape: ", len(encodedText))
 
-	// Inspect the first 1000 characters (or fewer if the text is shorter)
-	// if len(encodedText) > 1000 {
-	// 	fmt.Println("First 1000 encoded characters:", encodedText[:1000])
-	// } else {
-	// 	fmt.Println("First 1000 encoded characters:", encodedText)
-	// }
-
-	// now split up the data into train and validation sets
-	// first 90% will be train, rest val
 	trainSize := int(0.9 * float64(len(encodedText)))
 	train_data := encodedText[:trainSize]
 	val_data := encodedText[trainSize:]
@@ -114,4 +112,46 @@ func main() {
 	fmt.Println("Train data size: ", len(train_data))
 	fmt.Println("Val data size: ", len(val_data))
 
+	block_size := 8
+	batch_size := 4
+
+	x, y := get_batch("train", train_data, val_data, batch_size, block_size)
+
+	fmt.Println("Batch inputs (x) shape:", x.Shape())
+	fmt.Println("Batch targets (y) shape:", y.Shape())
+	fmt.Println("First batch inputs (x):", x)
+	fmt.Println("First batch targets (y):", y)
+	fmt.Println("-------------------")
+
+	for b := 0; b < batch_size; b++ {
+		for t := 0; t < block_size; t++ {
+			// Slice and handle context correctly
+			contextView, err := x.Slice(gorgonia.S(b), gorgonia.S(0, t+1))
+			if err != nil {
+				fmt.Println("Error slicing tensor x:", err)
+				continue
+			}
+			contextData := contextView.Data()
+
+			var context []int
+			switch v := contextData.(type) {
+			case []int:
+				context = v
+			case int:
+				context = []int{v}
+			default:
+				fmt.Println("Unexpected type for context:", v)
+				continue
+			}
+
+			targetView, err := y.At(b, t)
+			if err != nil {
+				fmt.Println("Error getting tensor y at index:", err)
+				continue
+			}
+			target := targetView.(int)
+
+			fmt.Printf("When input is %v the target: %d\n", context, target)
+		}
+	}
 }
