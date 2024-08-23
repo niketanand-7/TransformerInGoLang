034059c6c477d21a6c58bbe2d0ff6642b0ad74d4
niketanand-7 <niketanand7@gmail.com>
2024-08-23 12:26:19 -0400

    Add data loading function

--- transformer/nGramModel.py
+++ transformer/nGramModel.py
@@ -32,3 +32,13 @@ data = torch.tensor(encode(text), dtype=torch.long)
 n = int(0.9*len(data)) # first 90% will be train, rest val
 train_data = data[:n]
 val_data = data[n:]
+
+# data loading
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
