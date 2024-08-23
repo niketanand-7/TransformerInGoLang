a9544f160aea7047ac9d34259d25839af53af12a
niketanand-7 <niketanand7@gmail.com>
2024-08-23 12:26:06 -0400

    Add train and test splits to nGramModel.py

--- transformer/nGramModel.py
+++ transformer/nGramModel.py
@@ -26,3 +26,9 @@ stoi = { ch:i for i,ch in enumerate(chars) }
 itos = { i:ch for i,ch in enumerate(chars) }
 encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
 decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+
+# Train and test splits
+data = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9*len(data)) # first 90% will be train, rest val
+train_data = data[:n]
+val_data = data[n:]
